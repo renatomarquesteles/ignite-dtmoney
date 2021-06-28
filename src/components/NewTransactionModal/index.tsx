50ec623a7c1cdebab8baa91bbf6f800cfbda716b
Renato Marques <renatomarquesteles@gmail.com>
2021-06-28 01:05:34 -0300

    Salvando dados do form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import ReactModal from 'react-modal';
 
 import incomeImg from '../../assets/income.svg';
@@ -16,6 +16,13 @@ export function NewTransactionModal({
   onRequestClose
 }: NewTransactionModalProps) {
   const [type, setType] = useState('deposit');
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+  }
 
   return (
     <ReactModal
@@ -32,16 +39,20 @@ export function NewTransactionModal({
         <img src={closeImg} alt="Fechar modal"/>
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
         <input
           placeholder="Título"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
         />
         
         <input
           type="number"
           placeholder="Valor"
+          value={value}
+          onChange={event => setValue(+event.target.value)}
         />
 
         <TransactionTypeContainer>
@@ -68,6 +79,8 @@ export function NewTransactionModal({
 
         <input
           placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
         />
 
         <button type="submit">
